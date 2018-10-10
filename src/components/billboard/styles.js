f75ee50bb86723402ddaf5fd017259f0bed9de62
IveTou <ive_tourinho@hotmail.com>
2018-10-10 03:31:42 -0300

    Time and Place

--- src/components/billboard/styles.js
+++ src/components/billboard/styles.js
@@ -1,103 +1,40 @@
 import { withStyles } from '@material-ui/core/';
+import { deepOrange } from '@material-ui/core/colors';
 
 const calendarBorder = '2px solid rgba(255, 255, 255,.26)';
 
 export const withIndexStyle = withStyles(theme => ({
   root: {
-    backgroundColor: theme.palette.primary.light,
-    borderRadius: theme.spacing.unit / 2,
-    width: '100%',
     height: theme.spacing.unit * 40,
+    background: theme.palette.grey[200],
+    [theme.breakpoints.down('sm')]: {
+      height: theme.spacing.unit * 36,
+    },
+  },
+  flyer: {
+    height: '100%',
+    width: '100%',
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'cover',
     whiteSpace: 'nowrap',
-    position: 'relative',
-    display: 'block',
-    [theme.breakpoints.down('sm')]: {
-      height: theme.spacing.unit * 36,
-      backgroundColor: theme.palette.secondary.main,
-    },
   },
-  miniRoot: {
-    height: theme.spacing.unit * 30,
-    [theme.breakpoints.down('sm')]: {
-      height: theme.spacing.unit * 20,
-    },
-  },
-  overlay: {
-    height: '100%',
-    minWidth: '60%',
-    borderRadius: theme.spacing.unit / 2,
-    backgroundColor: theme.palette.action.active,
-    color: theme.palette.grey[50],
+  content: {
     padding: theme.spacing.unit * 2,
-    display: 'inline-block',
-    float: 'right',
-  },
-  miniOverlay : {
-    width: '60%',
-    padding: theme.spacing.unit,
-  },
-  title: {
-    overflow: 'hidden',
-    whiteSpace: 'normal',
-    maxHeight: '50%',
-    fontSize: theme.typography.display1.fontSize,
-    [theme.breakpoints.down('sm')]: {
-      fontSize: theme.typography.headline.fontSize,
-    },
-  },
-  subtitle: {
-    overflow: 'hidden',
-    whiteSpace: 'normal',
-    maxHeight: '100%',
-    padding: theme.spacing.unit,
-    fontSize: theme.typography.title.fontSize,
-    fontWeight: theme.typography.fontWeightLight,
-    [theme.breakpoints.down('sm')]: {
-      fontSize: theme.typography.subheading.fontSize,
-    },
-  },
-  miniSubtitle :{
-    fontWeight: theme.typography.fontWeightRegular,
-  },
-  placeTime: {
-    display: 'inline-flex',
-  },
-  calendar: {
-    float: 'left',
-    marginRight: theme.spacing.unit,
-    paddingRight: theme.spacing.unit,
-    paddingTop: theme.spacing.unit,
-    borderRight: calendarBorder,
   },
   timePlace: {
-    paddingTop: theme.spacing.unit,
-  },
-  avatar: {
-    marginRight: theme.spacing.unit,
-  },
-  list: {
-    display: 'flow-root',
-    paddingTop: 0,
-  },
-  listItem: {
-    padding: theme.spacing.unit,
-    paddingTop: 0,
-  },
-  link: {
-    textDecorationLine: 'inherit',
-    color: theme.palette.common.white,
-  },
-  action: {
+    height: '100%',
+    display: 'inline-flex',
     paddingTop: theme.spacing.unit * 2,
-    display: 'flex',
-    justifyContent: 'flex-end',
-    [theme.breakpoints.down('sm')]: {
-      justifyContent: 'space-between',
-    },
   },
-  button: {
-    border: `1px solid ${theme.palette.common.white}`,
+  listItemText: {
+    fontSize: theme.typography.subheading.fontSize,
+  },
+  calendar: {
+    borderRight: `2px solid ${theme.palette.grey[500]}`,
+    paddingRight: theme.spacing.unit,
+  },
+  actions: {
+    background: deepOrange[100],
+    height: theme.spacing.unit * 4,
   },
 }));
